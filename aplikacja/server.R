e8b9b07d6553127dc4523e57f95d80b9c09c5de8
p-przybylek <pp.paulinaprzybylek@gmail.com>
2020-11-17 08:30:59 +0100

    możliwość przejścia z kafelka lodówka do drugiego interfejsu aplikacji

--- aplikacja/server.R
+++ aplikacja/server.R
@@ -1,6 +1,12 @@
 library(shiny)
-
+library(shinyjs)
+library(shinydashboard)
 
 shinyServer(function(input, output, session) {
-
+  onclick("box-fridge", {
+    newtab <- switch(input$tabs,
+                     "main" = "models",
+                     "models" = "main")
+    updateTabItems(session, "tabs", newtab)
+  })
 })
--- aplikacja/ui.R
+++ aplikacja/ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyjs)
 library(shinydashboard)
 
 shinyUI(
@@ -9,23 +10,31 @@ shinyUI(
                                   dropdownMenu(type = NULL, badgeStatus = NULL, icon = icon("cog")),
                                   dropdownMenu(type = NULL, badgeStatus = NULL, icon = icon("sign-out-alt"))),
                   dashboardSidebar(disable = TRUE,
-                                   sidebarMenu(menuItem("Strona główna", tabName = "main"),
+                                   sidebarMenu(id = "tabs",
+                                               menuItem("Strona główna", tabName = "main"),
                                                menuItem("Porównanie kosztów zużycia energii", tabName = "models"),
                                                menuItem("Oferty", tabName = "oferts"))),
                   dashboardBody(
+                    useShinyjs(),
                     tags[["link"]](rel = "stylesheet", type = "text/css", href = "styl.css"),
                     tabItems(
                       tabItem(tabName = "main",
-                              fluidRow(column(12, align="center", selectInput("sorting", NULL, choices = c("Najtańsze wymiany", "Najbardziej opłacalne wymiany", "Najbardziej energooszczędne wymiany")))),
+                              fluidRow(column(12, align = "center", selectInput("sorting", NULL, choices = c("Najtańsze wymiany", "Najbardziej opłacalne wymiany", "Najbardziej energooszczędne wymiany")))),
                               div(id = "box-page",
                               fluidRow(
-                                box(width = 4),
-                                box(width = 4),
-                                box(width = 4)),
+                                box(width = 4, class = "box", id = "box-fridge",
+                                    p("LODÓWKI")),
+                                box(width = 4, class = "box", id = "box-tv",
+                                    p("TELEWIZORY")),
+                                box(width = 4, class = "box", id = "box-kettle",
+                                    p("CZAJNIKI"))),
                               fluidRow(
-                                box(width = 4),
-                                box(width = 4),
-                                box(width = 4)))),
+                                box(width = 4, class = "box", id = "box-washing-machine",
+                                    p("PRALKI")),
+                                box(width = 4, class = "box", id = "box-air-conditioning",
+                                    p("KLIMATYZACJE")),
+                                box(width = 4, class = "box", id = "box-microwave",
+                                    p("MIKROFALÓWKI"))))),
                       tabItem(tabName = "models"),
                       tabItem(tabName = "oferts")))),
     tags[["footer"]]("Naatu Energy", class = "footer")))
