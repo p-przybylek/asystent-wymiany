8709ec77d7d0bdbaeb372c2c7fd8ab257e8d27a0
DTGT <themegatgt48@gmail.com>
2020-11-18 15:02:36 +0100

    Dzialajacy CSS i dodatkowe META pliki

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,56 +1,71 @@
+#' The application User-Interface
+#' 
+#' @param request Internal parameter for `{shiny}`. 
+#'     DO NOT REMOVE.
 #' @import shiny
-app_ui <- function() {
+#' @noRd
+app_ui <- function(request) {
   tagList(
+    # Leave this function for adding external resources
+    golem_add_external_resources(),
     shinydashboard::dashboardPage(title = "Asystent Wymiany",
                                   shinydashboard::dashboardHeader(title = "ASYSTENT WYMIANY SPRZĘTU RTV I AGD",
-                                  titleWidth = 600,
-                                  shinydashboard::dropdownMenu(type = NULL, badgeStatus = NULL, icon = icon("cog")),
-                                  shinydashboard::dropdownMenu(type = NULL, badgeStatus = NULL, icon = icon("sign-out-alt"))),
+                                                                  titleWidth = 600,
+                                                                  shinydashboard::dropdownMenu(type = NULL, badgeStatus = NULL, icon = icon("cog")),
+                                                                  shinydashboard::dropdownMenu(type = NULL, badgeStatus = NULL, icon = icon("sign-out-alt"))),
                                   shinydashboard::dashboardSidebar(disable = TRUE,
                                                                    shinydashboard::sidebarMenu(id = "tabs",
                                                                                                shinydashboard::menuItem("Strona główna", tabName = "main"),
                                                                                                shinydashboard::menuItem("Porównanie kosztów zużycia energii", tabName = "models"),
                                                                                                shinydashboard::menuItem("Oferty", tabName = "oferts"))),
                                   shinydashboard::dashboardBody(
-                                  shinyjs::useShinyjs(),
-                    tags[["link"]](rel = "stylesheet", type = "text/css", href = "www/styl.css"),
-                    shinydashboard::tabItems(
-                      shinydashboard::tabItem(tabName = "main",
-                              fluidRow(column(12, align = "center", selectInput("sorting", NULL, choices = c("Najtańsze wymiany", "Najbardziej opłacalne wymiany", "Najbardziej energooszczędne wymiany")))),
-                              div(id = "box-page",
-                                  fluidRow(
-                                    shinydashboard::box(width = 4, class = "box", id = "box-fridge",
-                                        p("LODÓWKI")),
-                                    shinydashboard::box(width = 4, class = "box", id = "box-tv",
-                                        p("TELEWIZORY")),
-                                    shinydashboard::box(width = 4, class = "box", id = "box-kettle",
-                                        p("CZAJNIKI"))),
-                                  fluidRow(
-                                    shinydashboard::box(width = 4, class = "box", id = "box-washing-machine",
-                                        p("PRALKI")),
-                                    shinydashboard::box(width = 4, class = "box", id = "box-air-conditioning",
-                                        p("KLIMATYZACJE")),
-                                    shinydashboard::box(width = 4, class = "box", id = "box-microwave",
-                                        p("MIKROFALÓWKI"))))),
-                      shinydashboard::tabItem(tabName = "models"),
-                      shinydashboard::tabItem(tabName = "oferts")))),
-    tags[["footer"]]("Naatu Energy", class = "footer"))
+                                    shinyjs::useShinyjs(),
+                                    shinydashboard::tabItems(
+                                      shinydashboard::tabItem(tabName = "main",
+                                                              fluidRow(column(12, align = "center", selectInput("sorting", NULL, choices = c("Najtańsze wymiany", "Najbardziej opłacalne wymiany", "Najbardziej energooszczędne wymiany")))),
+                                                              div(id = "box-page",
+                                                                  fluidRow(
+                                                                    shinydashboard::box(width = 4, class = "box", id = "box-fridge",
+                                                                                        p("LODÓWKI")),
+                                                                    shinydashboard::box(width = 4, class = "box", id = "box-tv",
+                                                                                        p("TELEWIZORY")),
+                                                                    shinydashboard::box(width = 4, class = "box", id = "box-kettle",
+                                                                                        p("CZAJNIKI"))),
+                                                                  fluidRow(
+                                                                    shinydashboard::box(width = 4, class = "box", id = "box-washing-machine",
+                                                                                        p("PRALKI")),
+                                                                    shinydashboard::box(width = 4, class = "box", id = "box-air-conditioning",
+                                                                                        p("KLIMATYZACJE")),
+                                                                    shinydashboard::box(width = 4, class = "box", id = "box-microwave",
+                                                                                        p("MIKROFALÓWKI"))))),
+                                      shinydashboard::tabItem(tabName = "models"),
+                                      shinydashboard::tabItem(tabName = "oferts")))),
+    tags[["footer"]]("Naatu Energy", class = "footer")
+  )
 }
 
+#' Add external Resources to the Application
+#' 
+#' This function is internally used to add external 
+#' resources inside the Shiny application. 
+#' 
 #' @import shiny
+#' @importFrom golem add_resource_path activate_js favicon bundle_resources
+#' @noRd
 golem_add_external_resources <- function(){
   
-  addResourcePath(
+  add_resource_path(
     'www', app_sys('app/www')
   )
  
   tags$head(
-    golem::activate_js(),
-    golem::favicon(),
-    # Add here all the external resources
-    # If you have a custom.css in the inst/app/www
-    # Or for example, you can add shinyalert::useShinyalert() here
-    tags$link(rel="stylesheet", type="text/css", href="www/styl.css"), 
-    tags$script(src="www/script.js")
+    favicon(),
+    bundle_resources(
+      path = app_sys('app/www'),
+      app_title = 'asystentWymiany'
+    )
+    # Add here other external resources
+    # for example, you can add shinyalert::useShinyalert() 
   )
 }
+
--- R/run_app.R
+++ R/run_app.R
@@ -1,20 +1,18 @@
-options(shiny.maxRequestSize = 10*1024^2) # W celu rozszezenia maksymalnego rozmiaru pliku mozliwego do przetwozenia przez shiny
-
-library(shiny)
-library(shinyjs)
-library(shinydashboard)
-
 #' Run the Shiny Application
+#'
+#' @param ... A series of options to be used inside the app.
 #'
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
-run_app <- function() {
+run_app <- function(
+  ...
+) {
   with_golem_options(
     app = shinyApp(
       ui = app_ui, 
       server = app_server
     ), 
-    golem_opts = list()
+    golem_opts = list(...)
   )
 }
