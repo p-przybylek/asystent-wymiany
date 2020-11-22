784b5bf3c099992faf4637565ba5899f95af0304
MrDomani <pmorgen99@gmail.com>
2020-11-22 18:21:17 +0100

    added tests for get_years_to_go()

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -3,5 +3,9 @@ new_m_power <- 170 # kWh/rok
 new_m_price <- 1200 # PLN
 el_cost <- 0.617 # PLN/kWh, średni koszt energii elektrycznej w Polsce (maj 2020)
 
-testthat("Plot works",
+test_that('get_years_to_go works',
+          expect_equal(get_time_to_go(cur_m_power = 100, new_m_power = 50, new_m_price = 0, el_cost = 1),0),
+          expect_equal(get_time_to_go(cur_m_power = 100, new_m_power = 50, new_m_price = 100, el_cost = 1),2))
+
+test_that("Plot works",
          expect_s3_class(yearly_forecast_plot(cur_m_power, new_m_power, new_m_price, el_cost),'ggplot'))
