4daa80a3a548029dc05ee4cd61bfed5b25040364
Piotr Sieńko <piotrsienk@gmail.com>
2020-12-10 15:44:47 +0100

    Dodane sprawdzanie danych TV

--- R/poprawnosc_danych.R
+++ R/poprawnosc_danych.R
@@ -97,7 +97,111 @@ sprawdz_poprawnosc_lodowek <- function(fridges){
 }
 
 
-
-sprawdz_poprawnosc_tv <- function(fridges){ #TODO()
-  TRUE
+#' @param tvs dataframe with tv models
+#' @return boolean
+sprawdz_poprawnosc_tv <- function(tvs){
+  
+  poprawnosc_tv <- TRUE
+  
+  if(any(is.na(tvs))){
+    print("W zbiorze są NA")
+    return(FALSE)
+  }
+  
+  if(!all(tvs$ID == 1:dim(tvs)[1])){
+    print("ID nie są kolejnymi liczbami od 1")
+    poprawnosc_tv <- FALSE
+  }
+  
+  if(!all(stringi::stri_sub(tvs$Nazwa, 1, 10) == rep("Telewizor ", dim(tvs)[1]))){
+    print("nazwy modeli powinny zaczynać się od 'Telewizor '")
+    poprawnosc_tv <- FALSE
+  }
+  if(!all(tvs$Cena > 0)){
+    print("wszystkie modele powinny coś kosztować")
+    poprawnosc_tv <- FALSE
+  }
+  if(!all(tvs$Pobor_mocy_tryb_czuwania_W > 0)){
+    print("wszystkie modele powinny zużywać prąd w trybie czuwania")
+    poprawnosc_tv <- FALSE
+  }
+  if(!all(tvs$Pobor_mocy_tryb_wlaczenia_W > tvs$Pobor_mocy_tryb_czuwania_W)){
+    print("wszystkie modele powinny zużywać wiecej pradu w trybie dzialania niż w trybie czuwania")
+    poprawnosc_tv <- FALSE
+  }
+  
+  if(class(tvs$Zdj) != "character"){
+    print("linki do zdjęć powinny być napisami")
+    poprawnosc_tv <- FALSE
+  }
+  if(class(tvs$Firma) != "character"){
+    print("firmy powinny być napisami")
+    poprawnosc_tv <- FALSE
+  }
+  if(!all(tvs$Przekatna_ekranu_cal > 0)){
+    print("Telewizor powininen mieć dodatnią przekątną ekranu")
+    poprawnosc_tv <- FALSE
+  }
+  if(class(tvs$Standard_ekranu) != "character"){
+    print("TV powinien mieć wpisaną klasę obrazu")
+    poprawnosc_tv <- FALSE
+  }
+  if(!all(tvs$Szerokosc_ekranu_px > 0)){
+    print("TV powinien mieć dodatnią szerokość ekranu")
+    poprawnosc_tv <- FALSE
+  }
+  
+  if(!all(tvs$Wysokosc_ekranu_px > 0)){
+    print("TV powinien mieć dodatnią wysokość ekranu")
+    poprawnosc_tv <- FALSE
+  }
+  
+  if(!all(match(unique(tvs$Smart_TV), c("Tak", "Nie"), nomatch = 0) > 0)){
+    print("Powinno być określone, czy Smart_TV jest obecne w telewizorze")
+    poprawnosc_tv <- FALSE
+  }
+  
+  if(!all(match(unique(tvs$Android_TV), c("Tak", "Nie"), nomatch = 0) > 0)){
+    print("Powinno być określone, czy Android_TV jest obecne w telewizorze")
+    poprawnosc_tv <- FALSE
+  }
+  
+  if(!all(match(unique(tvs$Dla_graczy), c("Tak", "Nie"), nomatch = 0) > 0)){
+    print("Powinno być określone, czy telewizor jest dla graczy")
+    poprawnosc_tv <- FALSE
+  }
+  
+  if(!all(match(unique(tvs$Technologia_HDR_High_Dynamic_Range), c("Tak", "Nie"), nomatch = 0) > 0)){
+    print("Powinno być określone, czy technologia HDR jest obecna w telewizorze")
+    poprawnosc_tv <- FALSE
+  }
+  
+  
+  if(class(tvs$Zlacza_HDMI) != "numeric"){
+    print("Powinna być określona liczba złącz HDMI w telewizorze")
+    poprawnosc_tv <- FALSE
+  }
+  
+  if(class(tvs$Zlacza_USB) != "numeric"){
+    print("Powinna być określona liczba złącz USB w telewizorze")
+    poprawnosc_tv <- FALSE
+  }
+  
+  if(class(tvs$Kolor_obudowy) != "character"){
+    print("Kolor obudowy powinien być wyrażony słownie")
+    poprawnosc_tv <- FALSE
+  }
+  
+  if(!all(match(unique(tvs$Klasa_energetyczna),
+                c("G", "F", "E", "D", "C", "B", "A", "A+", "A++", "A+++"), nomatch = 0) > 0)){
+    print("Klasa energetyczna powinna być wyrażona za pomocą oficjalnych oznaczeń")
+    poprawnosc_tv <- FALSE
+  }
+  
+  if(class(tvs$Czestotliwosc_odswiezania_ekranu_Hz) != "numeric"){
+    print("Powinna być określona liczbowo częstotliwość odświeżania ekranu")
+    poprawnosc_tv <- FALSE
+  }
+  
+  return(poprawnosc_tv)
 }
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -85,3 +85,105 @@ test_that('fridges są poprawne',{
   utils::data('fridges', package = 'asystentWymiany', envir = rlang::current_env())
   expect_true(sprawdz_poprawnosc_lodowek(fridges))
 })
+
+ddpcr::quiet(
+  test_that('sprawdz_poprawnosc_tv wykryje bledne dane',{
+    
+    utils::data('tvs', package = 'asystentWymiany', envir = rlang::current_env())
+    poprawne_dane <- tvs
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,1] <- NA
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,1] <- 0
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[2,2] <- "Bez 'Telewizor' na poczatku"
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,3] <- 0
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,4] <- 0
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,5] <- 0
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,4] <- 10
+    bledne_dane[1,5] <- 5
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[,6] <- rep(0, length(bledne_dane[,6]))
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,7] <- NA
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,8] <- 0
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[,9] <- rep(0, length(bledne_dane[,9]))
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,10] <- -1
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,11] <- 0
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,12] <- "idk"
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,13] <- -1
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,14] <- "chyba tak"
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,15] <- -1
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[1,16] <- "TRUE"
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[,17] <- "duzo"
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[,18] <- rep(1, length(bledne_dane[,18]))
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[,19] <- "extra"
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+    bledne_dane <- poprawne_dane
+    bledne_dane[,20] <- "a"
+    expect_false(sprawdz_poprawnosc_tv(bledne_dane))
+    
+  }))
+
+test_that('tvs są poprawne',{
+  utils::data('tvs', package = 'asystentWymiany', envir = rlang::current_env())
+  expect_true(sprawdz_poprawnosc_tv(tvs))
+})
